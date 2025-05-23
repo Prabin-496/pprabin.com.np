5aef935e983d66a49030ee4bd98668728f49fd63
Prabin _496 <prabinparajuli496@gmail.com>
2025-05-23 16:52:58 +0545

    new update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-    content: [
-      './index.html',
-      './src/**/*.{js,ts,jsx,tsx}',
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-    safelist: [
-      'bg-white/30',
-      'bg-gray-800/30',
-      'border-gray-200/30',
-      'border-gray-700/30',
-    ],
-  };
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+  
